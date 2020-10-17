70fc47a9327c8d49c94113212c2bca66eefd7de2
Torben <t>
2020-10-17 13:38:06 +0200

    Move MouseLook, add basic raycasting.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
 
+    // Once per frame
     private void Update()
     {
         var xPos = Input.GetAxis("Horizontal");
@@ -14,5 +15,18 @@ public class PlayerMovement : MonoBehaviour
         var movement = transform.right * xPos + transform.forward * zPos;
 
         controller.Move(movement * speed * Time.deltaTime);
+    }
+
+    // Physics engine update
+    void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+#if UNITY_EDITOR
+        Debug.DrawRay(transform.position, fwd, Color.green);
+#endif
+
+        if (Physics.Raycast(transform.position, fwd, 10))
+            print("There is something in front of the object!");
     }
 }
