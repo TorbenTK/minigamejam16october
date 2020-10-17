74d84e5ac8c4fbef3eea1cc23edc162308ab0f69
Sascha Klomp <sascha.klomp@ogd.nl>
2020-10-17 10:19:07 +0200

    Add floating animation script

---
+++ Assets/Scripts/floatingAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Floater v0.0.2
+// by Donovan Keith
+//
+// [MIT License](https://opensource.org/licenses/MIT)
+public class floatingAnimation : MonoBehaviour
+{
+    // User Inputs
+    public float degreesPerSecond = 15.0f;
+
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // Position Storage Variables
+    private Vector3 posOffset = new Vector3();
+
+    private Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    private void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
