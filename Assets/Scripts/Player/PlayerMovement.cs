eb7a78e08d0702a0904779a680810927ef66b79e
Torben <t>
2020-10-17 13:28:44 +0200

    Clean up, create new prefabs for managers and ui.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+
+    private void Update()
+    {
+        var xPos = Input.GetAxis("Horizontal");
+        var zPos = Input.GetAxis("Vertical");
+
+        var movement = transform.right * xPos + transform.forward * zPos;
+
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+}
