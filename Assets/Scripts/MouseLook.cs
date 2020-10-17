3564a2e24989bd8c512f5998b3543cf1e21e4480
Sascha Klomp <sascha.klomp@ogd.nl>
2020-10-17 12:59:08 +0200

    Add player movement and view

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity = 100f;
+    public Transform playerBody;
+
+    private float xRotation;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        var mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
