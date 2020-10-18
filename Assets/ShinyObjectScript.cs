75f252bcfbac0520b04cc9e46435c89159cb3a5c
Sascha Klomp <sascha.klomp@ogd.nl>
2020-10-18 21:22:49 +0200

    Add more shiny plus particle indicator

---
+++ Assets/ShinyObjectScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShinyObjectScript : MonoBehaviour
+{
+    public GameObject _sparkleEffect;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var spawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        var defaultRotation = new Quaternion(0, 0, 0, 0);
+        var sparkles = Instantiate(_sparkleEffect, spawnPosition, defaultRotation);
+        Destroy(sparkles, 2.0f);
+    }
+}
